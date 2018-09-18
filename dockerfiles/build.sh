9683748d0779576f08443c99701cbf978e78eb30
Jared Allard <jaredallard@outlook.com>
2018-09-17 23:14:26 -0700

    chore: v1.0.1

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Build the containers
 
-docker build -t jaredallard/chromium-rpi:v1.0.0 -f chromium.Dockerfile .
+docker build -t jaredallard/chromium-rpi:v1.0.1 -f chromium.Dockerfile .
 docker build -t jaredallard/xorg-rpi:v1.0.0 -f xorg.Dockerfile .
 
-docker push jaredallard/chromium-rpi:v1.0.0
+docker push jaredallard/chromium-rpi:v1.0.1
 docker push jaredallard/xorg-rpi:v1.0.0
