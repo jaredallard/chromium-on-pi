56f4e1bd992187de960e4a14a10c1cb5e39a4496
Jared Allard <jaredallard@outlook.com>
2018-09-17 23:15:57 -0700

    fix: v1.0.1 xorg

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -2,7 +2,7 @@
 # Build the containers
 
 docker build -t jaredallard/chromium-rpi:v1.0.1 -f chromium.Dockerfile .
-docker build -t jaredallard/xorg-rpi:v1.0.0 -f xorg.Dockerfile .
+docker build -t jaredallard/xorg-rpi:v1.0.1 -f xorg.Dockerfile .
 
 docker push jaredallard/chromium-rpi:v1.0.1
-docker push jaredallard/xorg-rpi:v1.0.0
+docker push jaredallard/xorg-rpi:v1.0.1
